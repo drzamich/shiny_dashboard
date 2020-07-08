d6190c28cb9187cf818ea24bea28025b48b141ba
Michal Drzazga <drzamich@gmail.com>
2020-07-08 19:18:54 +0200

    feat: limit locations in generated data

--- data/fake_data_generator.py
+++ data/fake_data_generator.py
@@ -1,6 +1,8 @@
 from os import path
 import pandas as pd
 from datetime import datetime
+from random import random
+from math import floor
 from faker import Faker
 from faker.providers import date_time, python, geo
 
@@ -24,7 +26,9 @@ production_df.to_csv(directory + '/production.csv', index = False)
 
 # SALES
 rows = 2500
-locations = [faker.location_on_land() for i in range(rows)]
+no_locations = 13
+selected_locations = [faker.location_on_land() for i in range(no_locations)]
+locations = [selected_locations[floor(random() * no_locations)] for i in range(rows)]
 data = {
     'date': [faker.date_between_dates(datetime.fromisoformat(start_date), datetime.fromisoformat(end_date)) for i in range(rows)],
     'profit': [faker.pyfloat(min_value = -1000, max_value = 5000, right_digits=2) for i in range(rows)],
